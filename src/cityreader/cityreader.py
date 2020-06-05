936db7c0bd936dd41038ee7631a552a8e3bc25a2
Brandon-Harris <brandonharris177@gmail.com>
2020-06-05 10:40:32 -0600

    code appears correct but test is failing for some reason in cities file, also added note to oop file to indicate which class was the base

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,6 +1,15 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
 
+class City:
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
+
+  def __str__(self):
+    return "{self.name}, {self.lat}, {self.lon}".format(self=self)
+
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -20,7 +29,13 @@ def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
+
+    import csv
+    with open('cities.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            # print(row["city"], row["lat"], row["lng"])
+            cities.append(City(row["city"], row["lat"], row["lng"]))
     return cities
 
 cityreader(cities)
--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -18,7 +18,7 @@
 #
 # Put a comment noting which class is the base class
 
-class Vehicle:
+class Vehicle: #this is the base class
     pass
 
 class GroundVehicle(Vehicle):
